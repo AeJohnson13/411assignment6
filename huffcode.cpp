23addb910b18be44cbcc42b41cb18b239ee830dc
Alex Johnson <aej800@gmail.com>
2025-11-21 13:45:14 -0900

    going insane slowly

--- huffcode.cpp
+++ huffcode.cpp
@@ -11,70 +11,113 @@
 using std::string;
 #include <unordered_map>
 using std::unordered_map;
-#include <algorithm>
-using std::sort;
 #include <memory> 
-using std::unique_ptr; 
-using std::make_unique;
+using std::shared_ptr;
+using std::make_shared; 
 #include <vector>
 using std::vector;
 #include <utility>
-using std::move;
 using std::pair;
 #include <queue>
 using std::priority_queue;
-
-void HuffCode::setWeights(const unordered_map<char, int> & theweights) 
-{
-     priority_queue<unique_ptr<HuffCode>, vector<unique_ptr<HuffCode>>, compareNodes> HuffQueue;
-
-     for (auto iter = theweights.begin(); iter != theweights.end(); iter++)
-     {
-      
-        HuffQueue.push(make_unique<HuffCode>(string(1, iter->first), iter-> second));
-     }
-
-     while(HuffQueue.size() > 1)
-     {
-        auto left = move(HuffQueue.top()); 
-        HuffQueue.pop();
-        auto right = move(HuffQueue.top()); 
-        HuffQueue.pop();
-
-        int newFreq = nodeA->_freq + nodeB ->_freq;
-        string newData = nodeA->_data + nodeB ->_data;
-
-        unique_ptr<HuffCode> parentNode = make_unique<HuffCode>(newData, newFreq);
-        //parentNode->_left = nodeA;
-        //parentNode->_right = nodeB;
-
-        HuffQueue.push(parentNode);
-     }
-
-}
-
-
-string HuffCode::encode(const string & text) const
-{
-    // TODO: WRITE THIS!!!
-    return "";  // DUMMY
-}
-
-
-string HuffCode::decode(const string & codestr) const
-{
-    // TODO: WRITE THIS!!!
-    return "";  // DUMMY
-}
-
 
 
 // compareNodes
 // based on CmpEdgePtrs found here -> https://stackoverflow.com/questions/23997104/priority-queue-with-pointers-and-comparator-c
 struct compareNodes
 {
-    bool operator()(const unique_ptr<HuffCode> a, const unique_ptr<HuffCode> b) const
+    bool operator()(const shared_ptr<HuffCode> a, const shared_ptr<HuffCode> b) const
     {
         return a->_freq > b->_freq;
     }
 };
+
+void HuffCode::setWeights(const unordered_map<char, int> & theweights) 
+{
+     priority_queue<shared_ptr<HuffCode>, vector<shared_ptr<HuffCode>>, compareNodes> HuffQueue;
+
+     for (auto iter = theweights.begin(); iter != theweights.end(); iter++)
+     {
+      
+        HuffQueue.push(make_shared<HuffCode>(string(1, iter->first), iter-> second));
+     }
+
+     while(HuffQueue.size() > 1)
+     {
+        auto left = HuffQueue.top();
+        HuffQueue.pop();
+        auto right = HuffQueue.top();
+        HuffQueue.pop();
+
+        int newFreq = left->_freq + right ->_freq;
+        string newData = left->_data + right ->_data;
+
+        shared_ptr<HuffCode> parentNode = make_shared<HuffCode>(newData, newFreq);
+        parentNode->_left = left;
+        parentNode->_right = right;
+
+        HuffQueue.push(parentNode);
+    }
+
+    auto final = HuffQueue.top();
+    _data = final->_data,
+    _freq = final->_freq,
+    _left = final->_left,
+	_right = final->_right;
+}
+
+void HuffCode::Traverse(unordered_map<char, string> & dict, string code) const
+{
+    if(_left != nullptr)
+    {
+        code = code + "0";
+        _left->Traverse(dict, code);
+    }
+    if(_right != nullptr)
+    {
+        code = code + "1";
+        _right->Traverse(dict, code);
+    }
+    if(_right == nullptr && _left == nullptr){
+        char key = _data[0];
+        pair<char, string> newEntry = {key, code};
+        dict.insert(newEntry);
+    }
+}
+
+string HuffCode::encode(const string & text) const
+{
+    unordered_map<char, string> dict;
+    Traverse(dict, "");
+
+    string fullCode = "";
+    for(auto key : text){
+        auto charCode = dict.find(key)->second;
+        fullCode = fullCode + charCode;
+    }
+    return fullCode;
+}
+
+
+string HuffCode::decode(const string & codestr) const
+{
+auto root = *this;
+string decodedString = "";
+auto currNode = root;
+
+for(auto codeBit : codestr)
+    if(currNode._left == nullptr && currNode._right == nullptr)
+    {
+        decodedString = decodedString + currNode._data;
+        currNode = root;
+    }
+    else if(codeBit == '0')
+    {
+        currNode = *currNode._left;
+    }
+     else if(codeBit == '1')
+    {
+        currNode = *currNode._right;
+    }
+    return decodedString;
+}
--- huffcode.hpp
+++ huffcode.hpp
@@ -15,7 +15,8 @@
 #include <unordered_map>
 // For std::unordered_map;
 #include <memory> 
-// For std::unique_ptr, std::make_unique
+// For std::shared_ptr
+// For std::enable_shared_from_this
 
 
 //Forward Decleration so that compareNodes can be declared as friend later;
@@ -23,26 +24,41 @@ struct compareNodes;
 
 // Class HuffCode
 // Encoding & decoding using a Huffman code
-class HuffCode {
-
+class HuffCode
+ {
 // ***** HuffCode: ctors, dctor, op= *****
 public:
 
-    // Compiler-generated default ctor, copy ctor, copy =, dctor used
-    
-
+    // Compiler-generated copy ctor, copy =, dctor used
     friend struct compareNodes;
-    
+
     // 2-param ctor
 	// _data is set to given data
+    // _freq is set to give freq
 	// _left and _right are set to nullptr
 	// No-Throw Guarantee 
 	HuffCode(const std::string & data, const int & freq)             
 		:_data(data),
-        _freq(freq),
+       _freq(freq),
 		_left(nullptr),
 		_right(nullptr)
-    {}
+    {
+        //Left Empty Intentionally 
+    }
+
+    // default ctor
+	// _data is set to empty string
+    // _freq is set to zero 
+	// _left and _right are set to nullptr
+	// No-Throw Guarantee 
+    HuffCode()
+        :_data(""),
+       _freq(0),
+		_left(nullptr),
+		_right(nullptr)
+    {
+        //Left Empty Intentionally 
+    }
 // ***** HuffCode: general public functions *****
 public:
 
@@ -52,14 +68,14 @@ public:
 
     std::string decode(const std::string & codestr) const;
 
-    bool operator<(const HuffCode& other) const;
+    void Traverse(std::unordered_map<char, std::string> & dict, std::string code) const;
 
 // ***** HuffCode: data members *****
 private:
     std::string _data;  //Data for node
     int _freq;          //Frequency for node
-    std::unique_ptr<HuffCode> _left;    //Ptr to left node 
-	std::unique_ptr<HuffCode> _right;   //Ptr to right node
+    std::shared_ptr<HuffCode> _left;    //Ptr to left node 
+	std::shared_ptr<HuffCode> _right;   //Ptr to right node
 };  // End class HuffCode
 
 #endif //#ifndef FILE_HUFFCODE_HPP_INCLUDED
